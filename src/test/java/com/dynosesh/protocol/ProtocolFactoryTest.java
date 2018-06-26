cee23a461d7c0ab28a6807e63c99d16358c8681e
Rory Malcolm <RoryMcKenzieMalcolm@Gmail.com>
2018-06-26 10:22:38 +0100

    Simple code cleanup in ProtocolFactoryTest

--- src/test/java/com/dynosesh/protocol/ProtocolFactoryTest.java
+++ src/test/java/com/dynosesh/protocol/ProtocolFactoryTest.java
@@ -14,7 +14,7 @@ import static org.junit.Assert.assertTrue;
  */
 public class ProtocolFactoryTest {
 
-  ProtocolFactory protocolFactory;
+  private ProtocolFactory protocolFactory;
 
   @Before
   public void setUp() {
