89b0efb0a68957d51955acd9461264362b1a4aa1
Rory Malcolm <RoryMcKenzieMalcolm@Gmail.com>
2018-07-05 14:13:26 +0100

    Documentation update for Actor interface

--- src/main/java/com/dynosesh/actor/Actor.java
+++ src/main/java/com/dynosesh/actor/Actor.java
@@ -4,7 +4,17 @@ import com.dynosesh.Sendable;
 
 public interface Actor {
 
+  /**
+   * Sends a task to the actor specified
+   *
+   * @param sendable The task to send to the actor
+   */
   void sendTask(Sendable sendable);
 
+  /**
+   * Receives a task from the actor specified
+   *
+   * @return The task from the actor
+   */
   Sendable receiveTask();
 }
